3977cd8f58e326dd2c89f33279555c79188d3658
guyulei <1253908433@qq.com>
2017-11-13 08:59:20 +0800

    3

--- PracticeDraw3/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice11GetFontSpacingView.java
+++ PracticeDraw3/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice11GetFontSpacingView.java
@@ -35,9 +35,8 @@ public class Practice11GetFontSpacingView extends View {
         float spacing = 20;
         spacing += paint.getFontSpacing();
         canvas.drawText(text, 50, 100, paint);
-
         canvas.drawText(text, 50, 100 + spacing, paint);
-
         canvas.drawText(text, 50, 100 + spacing * 2, paint);
+
     }
 }
